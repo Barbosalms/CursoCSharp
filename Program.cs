f281eb8815bbb095033b2cee89813f5d76d87793
FELIPE BARBOSA DOS SANTOS <felipebbarbosasantos@gmail.com>
2023-05-31 17:17:58 -0300

    CursoCSharp

--- Program.cs
+++ Program.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using CursoCSharp.Fundamentos;
 using CursoCSharp.EstruturasDeControle;
 using CursoCSharp.ClassesEMetodos;
+using CursoCSharp.Colecoes;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                //Fundamentos
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Variaveis e Constantes - Fundamentos",VariaveisEConstantes.Executar },
                 {"Notação Ponto - Fundamentos ",NotacaoPonto.Executar },
@@ -17,11 +19,13 @@ namespace CursoCSharp {
                 {"Operadores Aritmetricos  - Fundamentos", OperadoresAritmeticos.Executar },
                 {"Exercício 1 - Fundamentos", Exercicios.Executar },
                 {"Exercício IF e ELSE - Fundamentos", ExercicoIfElse.Executar },
+                // Estruturas de Controle
                 {"Estrutura Switch - Estruturas de Controle", EstruturaSwitch.Executar},
                 {"Exercicio Switch - Estruturas de Controle", ExercicioSwitch.Executar},
                 {"Estrutura While e For - Estruturas de Controle", EstruturaWhileEFor.Executar},
                 {"Exercicio While e For - Estruturas de Controle", ExercicioRepeticao.Executar},
                 {"Estrutura Foreach - Estruturas de Controle", EstruturaForeach.Executar},
+                //Classes e Metodos
                 {"Classes e Metodos - Classes e Metodos", Membros.Executar},
                 {"Metodos com retorono - Classes e Metodos", MetodosComRetorno.Executar},
                 {"Exercicio Metodos e Classes - Classes e Metodos", ExerciciosMetodosComRetorno.Executar},
@@ -32,6 +36,9 @@ namespace CursoCSharp {
                 {"Exercicio 1 GET e SET  - Classes e Metodos", ExGetESet1.Executar},
                 {"Exercicio 2 GET e SET  - Classes e Metodos", ExGetESet2.Executar},
                 {"Exercicio 3 GET e SET  - Classes e Metodos", ExGetESet3.Executar},
+                //Colecoes
+                {"Arrays - Colecoes", Arrays.Executar},
+                {"Queue - Colecoes", ColecoesQueue.Executar},
                 
 
 
